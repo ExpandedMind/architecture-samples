8526716bef70c446c1c64a3e7e36f88077114923
Ivan Ramos <ivan.ramos@kroger.com>
2021-03-10 16:51:35 -0600

    All 3 tests implemented

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/chapter1/TasksActivityTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/chapter1/TasksActivityTest.kt
@@ -1,9 +1,13 @@
 package com.example.android.architecture.blueprints.todoapp.chapter1
 
+import android.media.MediaPlayer
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
@@ -54,14 +58,14 @@ class TasksActivityTest : BaseTest() {
         onView(withId(R.id.add_task_description_edit_text)).perform(ViewActions.typeText(taskDescription))
         onView(withId(R.id.save_task_fab)).perform(ViewActions.click())
         // Mark it completed
-        onView(withId(R.id.complete_checkbox)).perform(ViewActions.click())
+        onView(allOf(
+                withId(R.id.complete_checkbox),
+                hasSibling(withText(taskTitle))
+        )).perform(ViewActions.click())
         // Verify it is in the list of completed tasks
         onView(withId(R.id.menu_filter)).perform(ViewActions.click())
         onView(withText("Completed")).perform(ViewActions.click())
-        onView(withId(R.id.title_text)).check(matches(allOf(
-                isDisplayed(),
-                withText(taskTitle)
-        )))
+        onView(allOf(withId(R.id.title_text), withText(taskTitle))).check(matches(allOf(isCompletelyDisplayed(), withText(taskTitle))))
 
     }
 
@@ -70,7 +74,21 @@ class TasksActivityTest : BaseTest() {
      */
     @Test
     fun addNewTODO_editItAndSeeChanges() {
+        // Add a new TO-DO
+        val taskTitle = "Ocado testing"
+        val taskDescription = "Regression test Storeless experience"
 
+        onView(isAssignableFrom(FloatingActionButton::class.java)).perform(ViewActions.click())
+        onView(withId(R.id.add_task_title_edit_text)).perform(ViewActions.typeText(taskTitle))
+        onView(withId(R.id.add_task_description_edit_text)).perform(ViewActions.typeText(taskDescription))
+        onView(withId(R.id.save_task_fab)).perform(ViewActions.click())
+        // Edit it
+        onView(allOf(withId(R.id.title_text), withText(taskTitle))).perform(click())
+        onView(withId(R.id.edit_task_fab)).perform(click())
+        onView(withId(R.id.add_task_title_edit_text)).perform(replaceText("Storeless testing"))
+        onView(withId(R.id.save_task_fab)).perform(click())
+        // Verify modification
+        onView(allOf(withId(R.id.title_text), withText("Storeless testing"))).check(ViewAssertions.matches(isDisplayed()))
     }
 
 }
